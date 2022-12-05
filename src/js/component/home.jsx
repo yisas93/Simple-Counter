ad551e0acd6886ad923bc13804b80bc4d3680cbd
yisas93 <juan249325@gmail.com>
2022-12-05 14:49:12 +0000

    simple_counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState, useEffect } from "react";
+import Seconds from "./seconds.jsx";
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [seconds, setSeconds] = useState(0)
+	useEffect(()=>{
+		var intervalId = setInterval(()=>{
+			
+				setSeconds(seconds+1)
+				return clearInterval(intervalId)
+
+			},1000)
+			
+	})
+	
+	
+	
+	
+	return <div>
+		
+		<Seconds seconds={seconds} />
+	</div>
+}
 
 export default Home;
---
+++ src/js/component/seconds.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Seconds=(props)=>{
+
+
+    return <div className="container">
+        <div id="counter">{props.seconds}</div>
+        </div>
+
+}
+
+export default Seconds
